5c2b95913d64fbf9570a6f5cf6af4b42996702bd
redchiken <gombabo1217@gmail.com>
2016-08-30 20:40:20 +0900

    upload tutorial test

---
+++ UploadFile.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.POST("/upload", func(c *gin.Context) {
+
+		file, header, err := c.Request.FormFile("image")
+		if err != nil {
+			panic(err)
+		}
+		filename := header.Filename
+		fmt.Println(header.Filename)
+		out, err := os.Create("./tmp/" + filename + ".png")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer out.Close()
+		_, err = io.Copy(out, file)
+		if err != nil {
+			log.Fatal(err)
+		}
+	})
+	router.Run(":8080")
+}
