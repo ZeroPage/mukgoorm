9fc8848d40b05c7c7c82fad320fc1340fe384834
Jihye Seo <rabierre@gmail.com>
2016-12-04 18:31:32 +0900

    Fix event listener bug

--- static/javascript/list.js
+++ static/javascript/list.js
@@ -1,17 +1,19 @@
 window.onload = function() {
     var modal = document.getElementById('share-modal');
-    var shareBtn = document.getElementById('share-btn');
     var closeBtn = document.getElementById('close-modal-btn');
-
-    shareBtn.onclick = function(){
-        link = event.target.parentElement.getElementsByClassName("file-link")[0].href
-        modal.getElementsByClassName("link-holder")[0].textContent = link
-        modal.style.display = "block";
-    }
+    var shareBtns = document.getElementsByClassName('share-btn');
 
     closeBtn.onclick = function(){
         modal.style.display = "none";
     }
+
+    Array.from(shareBtns).forEach(function(element) {
+        element.onclick = function(event) {
+            link = event.target.parentElement.getElementsByClassName("file-link")[0].href
+            modal.getElementsByClassName("link-holder")[0].textContent = link
+            modal.style.display = "block";
+        }
+    });
 }
 
 function remove(filePath) {
