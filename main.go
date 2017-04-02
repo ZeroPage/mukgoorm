f68b558ad26f33bf5c529ad66c32c05cb2a6dab3
vkdjdldnjsw <vkdnjdldnjsw@naver.com>
2017-04-02 17:28:26 +0900

    add remoteDownload

--- main.go
+++ main.go
@@ -65,6 +65,7 @@ func NewEngine() *gin.Engine {
 	loginedRoute.POST("/upload", handlers.CheckRole(grant.ADMIN), handlers.Upload)
 	loginedRoute.DELETE("/delete", handlers.CheckRole(grant.ADMIN), handlers.Delete)
 	loginedRoute.GET("/search", handlers.CheckRole(grant.ADMIN, grant.READ_ONLY), handlers.Search)
+	loginedRoute.POST("/remoteDownload", handlers.CheckRole(grant.ADMIN), handlers.RemoteDownload)
 
 	return r
 }
