d4574a301695ff625b651d8e0d6147b5aca9c9c8
rabierre <rabierre@gmail.com>
2017-07-16 21:41:21 +0900

    Fix broken test

--- image/signature_test.go
+++ image/signature_test.go
@@ -1,19 +1,28 @@
 package image
 
 import (
+	"path"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/zeropage/mukgoorm/setting"
 )
 
+func init() {
+	dir := setting.GetDirectory()
+	dir.Path = "../testdata"
+}
+
 func TestJpg(t *testing.T) {
-	res := signature("../tmp/dat/.images/2017-07-09194024_flow-dual-ring-1-jpeg.jpg")
+	fileName := "pic.jpg"
+	res := signature(path.Join(setting.GetDirectory().Path, fileName))
 
 	assert.Equal(t, res, "jpeg")
 }
 
 func TestPng(t *testing.T) {
-	res := signature("../tmp/dat/2017-07-16172750_png_wolf_by_itsdura-d3cle9k.png")
+	fileName := "pic.png"
+	res := signature(path.Join(setting.GetDirectory().Path, fileName))
 
 	assert.Equal(t, res, "png")
 }
